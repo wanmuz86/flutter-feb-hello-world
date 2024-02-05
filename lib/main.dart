9c300227d8839d605726891563b8a1af9491db83
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-02-05 14:59:19 +0800

    add const for static declaration on variavle

--- lib/main.dart
+++ lib/main.dart
@@ -30,22 +30,22 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow,
-      appBar: AppBar(title: Text("Hello World"), backgroundColor: Colors.red,),
+      appBar: AppBar(title: const Text("Hello World"), backgroundColor: Colors.red,),
       body: SingleChildScrollView(
         child: Center(child: Column(
           children: [
             Text("Hello World", style: GoogleFonts.singleDay(
-              textStyle: TextStyle(color: Colors.red, fontSize: 40)
+              textStyle: const TextStyle(color: Colors.red, fontSize: 40)
             ),),
-            SizedBox(height: 10,),
+            const SizedBox(height: 10,),
             Text("Welcome to my app", style: GoogleFonts.bebasNeue(
-              textStyle: TextStyle(color: Colors.blue,
+              textStyle: const TextStyle(color: Colors.blue,
                   fontSize: 32)
             ),),
-            SizedBox(height: 10,),
-            Text("I hope you enjoy it", style: GoogleFonts.cuteFont(textStyle: TextStyle(color:Colors.green, fontSize: 24)),),
-            SizedBox(height: 10,),
-            Row(
+            const SizedBox(height: 10,),
+            Text("I hope you enjoy it", style: GoogleFonts.cuteFont(textStyle: const TextStyle(color:Colors.green, fontSize: 24)),),
+            const SizedBox(height: 10,),
+            const Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text("This is left"),
@@ -53,10 +53,10 @@ class HomePage extends StatelessWidget {
                 Text("This is right")
               ],
             ),
-            SizedBox(height: 10,),
+            const SizedBox(height: 10,),
             Image.network("https://cdn.audleytravel.com/4872/3480/79/264526-kuala-lumpur-by-night.jpg"),
             Image.asset("assets/photo.jpg"),
-            TextField(decoration: InputDecoration(hintText: "Enter your name"),
+            TextField(decoration: const InputDecoration(hintText: "Enter your name"),
             controller: nameEditingController,),
             TextButton(onPressed: (){
              // print("Hello ${nameEditingController.text}");
@@ -65,7 +65,7 @@ class HomePage extends StatelessWidget {
               var snackBar = SnackBar(content: Text("Hello ${nameEditingController.text}"));
               ScaffoldMessenger.of(context).showSnackBar(snackBar);
 
-            }, child: Text("Press me!"))
+            }, child: const Text("Press me!"))
 
           ],
         )
