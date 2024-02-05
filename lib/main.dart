8d9866402675d07f50f7a67e50f921927e9f7060
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-02-05 12:26:27 +0800

    add sizedbox

--- lib/main.dart
+++ lib/main.dart
@@ -22,32 +22,43 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  // Declaring a controller to be bind to the TextField
+ var nameEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow,
       appBar: AppBar(title: Text("Hello World"), backgroundColor: Colors.red,),
-      body: Center(child: Column(
-        children: [
-          Text("Hello World", style: TextStyle(color: Colors.red, fontSize: 40),),
-          SizedBox(height: 10,),
-          Text("Welcome to my app", style: TextStyle(color: Colors.blue,
-          fontSize: 32),),
-          SizedBox(height: 10,),
-          Text("I hope you enjoy it", style: TextStyle(color:Colors.green, fontSize: 24),),
-          SizedBox(height: 10,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("This is left"),
-              SizedBox(width: 10,),
-              Text("This is right")
-            ],
-          )
-        ],
-      )),
+      body: SingleChildScrollView(
+        child: Center(child: Column(
+          children: [
+            Text("Hello World", style: TextStyle(color: Colors.red, fontSize: 40),),
+            SizedBox(height: 10,),
+            Text("Welcome to my app", style: TextStyle(color: Colors.blue,
+            fontSize: 32),),
+            SizedBox(height: 10,),
+            Text("I hope you enjoy it", style: TextStyle(color:Colors.green, fontSize: 24),),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("This is left"),
+                SizedBox(width: 10,),
+                Text("This is right")
+              ],
+            ),
+            Image.network("https://cdn.audleytravel.com/4872/3480/79/264526-kuala-lumpur-by-night.jpg"),
+            TextField(decoration: InputDecoration(hintText: "Enter your name"),
+            controller: nameEditingController,),
+            TextButton(onPressed: (){
+              print("Hello ${nameEditingController.text}");
+            }, child: Text("Press me!"))
+
+          ],
+        )
+        ),
+      ),
     );
   }
 }
